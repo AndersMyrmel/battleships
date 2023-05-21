27bd5edac0c55edc4921434b8cd1f8c7b5e3c955
Anders <andersmyrmel@gmail.com>
2023-05-21 22:07:45 +0200

    clean up

--- client/src/context/SocketProvider.tsx
+++ client/src/context/SocketProvider.tsx
@@ -1,9 +1,6 @@
 import { createContext } from 'react';
 import { io, Socket } from 'socket.io-client';
 
-//const URL =
-//  process.env.NODE_ENV === 'production' ? undefined : 'http://localhost:4000';
-
 const URL = 'https://battleships-server-ia62.onrender.com/';
 const socket = io(URL, { transports: ['websocket', 'polling'] });
 const SocketContext = createContext<Socket>(socket);
